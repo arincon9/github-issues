758392804e3dc825a6deb4b2bd067938be2364c4
Alex Rincon <alejandro.rincon9@gmail.com>
2017-12-28 00:10:32 -0600

    Add argument parser

--- test/github_issues_test.exs
+++ test/github_issues_test.exs
@@ -2,7 +2,18 @@ defmodule GithubIssuesTest do
   use ExUnit.Case
   doctest GithubIssues
 
-  test "greets the world" do
-    assert GithubIssues.hello() == :world
+  import GithubIssues.CLI, only: [ parse_args: 1 ]
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args([ "-h",     "anything" ]) == :help
+    assert parse_args([ "--help", "anything" ]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
+  test "count is defaulted if two values given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
   end
 end
